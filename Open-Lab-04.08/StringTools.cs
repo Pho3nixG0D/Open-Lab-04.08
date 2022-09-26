57eb40a5c75522b58d08ee6fc255386f72529cdd
Pho3nixG0D <oliverkrskagmail.com>
2022-09-26 21:00:48 +0200

    counted length of word, created new array of words length, check if word have 4 chars than return result

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework.Internal.Execution;
+using System;
+using System.Diagnostics.Tracing;
 
 namespace Open_Lab_04._08
 {
@@ -6,7 +8,27 @@ namespace Open_Lab_04._08
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            int countl = 0;
+            int x = 0;
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].Length == 4)
+                {
+                    countl++;
+                }
+            }
+            string[] resultArray = new string[countl];
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].Length == 4)
+                {
+                    resultArray[x] = strings[i];
+                    x++;
+                }
+            }
+            return resultArray;
+
         }
     }
 }
