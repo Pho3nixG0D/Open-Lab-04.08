c69dd84429ff84f719115c9f4817dcba07f1d5b0
Pho3nixG0D <oliverkrskagmail.com>
2022-11-03 08:56:31 +0100

    Rebuild - Lambda

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework.Internal.Execution;
 using System;
+using System.Linq;
 using System.Diagnostics.Tracing;
 
 namespace Open_Lab_04._08
@@ -8,27 +9,7 @@ namespace Open_Lab_04._08
     {
         public string[] IsFourLetters(string[] strings)
         {
-            int countl = 0;
-            int x = 0;
-            for (int i = 0; i < strings.Length; i++)
-            {
-                if (strings[i].Length == 4)
-                {
-                    countl++;
-                }
-            }
-            string[] resultArray = new string[countl];
-
-            for (int i = 0; i < strings.Length; i++)
-            {
-                if (strings[i].Length == 4)
-                {
-                    resultArray[x] = strings[i];
-                    x++;
-                }
-            }
-            return resultArray;
-
+            return strings.Where(x => x.Length==4).ToArray();
         }
     }
 }
